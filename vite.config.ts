4a96ba06c86464ec86b9927a24b840344d51a878
yg <56058845+yunki-park@users.noreply.github.com>
2025-04-26 11:58:50 +0900

    [chore] src Path에 대한 alias 설정
    - 상대 경로 표현 대신 '@' alias 사용하도록 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteSingleFile } from 'vite-plugin-singlefile';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
