e64d550694f798e250c249ab7b1748144df7e128
yg <56058845+yunki-park@users.noreply.github.com>
2025-04-27 21:33:56 +0900

    [bugfix] 필터 기능 정상 동작하도록 추가 구현 및 안정화 - 필터 내 임시 필드 값 제거 및 실제 데이터 내에 포함된 필드값이 표시되도록 수정 - 제거된 레코드 내 필드 값이 필터에서 제거되지 않는 오류 수정   - useMemo를 사용하여 필터 내 데이터가 동적 업데이트되도록 수정   - `records`의 변화를 감지하여 필터 목록을 재생성하도록 구현

--- src/components/MemberTable/MemberTable.tsx
+++ src/components/MemberTable/MemberTable.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+
 import { Checkbox, Dropdown, Table, theme } from 'antd';
 import type { TableColumnsType } from 'antd';
 import { MoreOutlined } from '@ant-design/icons';
@@ -17,99 +19,110 @@ export const MemberTable: React.FC<MemberTableProps> = ({
 }) => {
   const { token } = theme.useToken();
 
-  const columns: TableColumnsType<MemberRecord> = [
-    {
-      title: '이름',
-      dataIndex: 'name',
-      filters: [
-        { text: 'John Doe', value: 'John Doe' },
-        { text: 'Foo Bar', value: 'Foo Bar' },
-      ],
-      onFilter: (value, record) => record.name.includes(value as string),
-    },
-    {
-      title: '주소',
-      dataIndex: 'address',
-      filters: [
-        { text: '서울 강남구', value: '서울 강남구' },
-        { text: '서울 서초구', value: '서울 서초구' },
-      ],
-      onFilter: (value, record) =>
-        record.address?.includes(value as string) ?? false,
-    },
-    {
-      title: '메모',
-      dataIndex: 'memo',
-      filters: [
-        { text: '외국인', value: '외국인' },
-        { text: '한국인', value: '한국인' },
-      ],
-      onFilter: (value, record) =>
-        record.memo?.includes(value as string) ?? false,
-    },
-    {
-      title: '가입일',
-      dataIndex: 'joinDate',
-      filters: [
-        { text: '2024-10-01', value: '2024-10-01' },
-        { text: '2024-10-02', value: '2024-10-02' },
-      ],
-      onFilter: (value, record) => record.joinDate.includes(value as string),
-    },
-    {
-      title: '직업',
-      dataIndex: 'job',
-      filters: [
-        { text: '개발자', value: '개발자' },
-        { text: 'PO', value: 'PO' },
-      ],
-      onFilter: (value, record) => record.job === value,
-    },
-    {
-      title: '이메일 수신 동의',
-      dataIndex: 'agreeToEmail',
-      filters: [
-        { text: '선택됨', value: true },
-        { text: '선택 안함', value: false },
-      ],
-      onFilter: (value, record) => record.agreeToEmail === value,
-      render: (agree: boolean) => (
-        <div style={{ cursor: 'not-allowed' }}>
-          <Checkbox checked={agree} style={{ pointerEvents: 'none' }} />
-        </div>
-      ),
-    },
-    {
-      key: 'actions',
-      render: (_, record) => (
-        <Dropdown
-          trigger={['click']}
-          menu={{
-            items: [
-              {
-                key: 'edit',
-                label: '수정',
-                onClick: () => {
-                  onEdit(record);
+  const columns = useMemo<TableColumnsType<MemberRecord>>(
+    () => [
+      {
+        title: '이름',
+        dataIndex: 'name',
+        filters: Array.from(new Set(records.map((r) => r.name))).map(
+          (name) => ({
+            text: name,
+            value: name,
+          }),
+        ),
+        onFilter: (value, record) => record.name.includes(value as string),
+      },
+      {
+        title: '주소',
+        dataIndex: 'address',
+        filters: Array.from(new Set(records.map((r) => r.address))).map(
+          (address) => ({
+            text: address,
+            value: address,
+          }),
+        ),
+        onFilter: (value, record) =>
+          record.address?.includes(value as string) ?? false,
+      },
+      {
+        title: '메모',
+        dataIndex: 'memo',
+        filters: Array.from(new Set(records.map((r) => r.memo))).map(
+          (memo) => ({
+            text: memo,
+            value: memo,
+          }),
+        ),
+        onFilter: (value, record) =>
+          record.memo?.includes(value as string) ?? false,
+      },
+      {
+        title: '가입일',
+        dataIndex: 'joinDate',
+        filters: Array.from(new Set(records.map((r) => r.joinDate))).map(
+          (date) => ({
+            text: date,
+            value: date,
+          }),
+        ),
+        onFilter: (value, record) => record.joinDate.includes(value as string),
+      },
+      {
+        title: '직업',
+        dataIndex: 'job',
+        filters: Array.from(new Set(records.map((r) => r.job))).map((job) => ({
+          text: job,
+          value: job,
+        })),
+        onFilter: (value, record) => record.job === value,
+      },
+      {
+        title: '이메일 수신 동의',
+        dataIndex: 'agreeToEmail',
+        filters: [
+          { text: '선택됨', value: true },
+          { text: '선택 안함', value: false },
+        ],
+        onFilter: (value, record) => record.agreeToEmail === value,
+        render: (agree: boolean) => (
+          <div style={{ cursor: 'not-allowed' }}>
+            <Checkbox checked={agree} style={{ pointerEvents: 'none' }} />
+          </div>
+        ),
+      },
+      {
+        key: 'actions',
+        render: (_, record) => (
+          <Dropdown
+            trigger={['click']}
+            menu={{
+              items: [
+                {
+                  key: 'edit',
+                  label: '수정',
+                  onClick: () => {
+                    onEdit(record);
+                  },
                 },
-              },
-              {
-                key: 'delete',
-                label: (
-                  <span style={{ color: `${token.colorError}` }}>삭제</span>
-                ),
-                onClick: () => {
-                  onDelete(record.key);
+                {
+                  key: 'delete',
+                  label: (
+                    <span style={{ color: `${token.colorError}` }}>삭제</span>
+                  ),
+                  onClick: () => {
+                    onDelete(record.key);
+                  },
                 },
-              },
-            ],
-          }}
-        >
-          <MoreOutlined style={{ cursor: 'pointer' }} />
-        </Dropdown>
-      ),
-    },
-  ];
+              ],
+            }}
+          >
+            <MoreOutlined style={{ cursor: 'pointer' }} />
+          </Dropdown>
+        ),
+      },
+    ],
+    [records, onEdit, onDelete, token.colorError],
+  );
 
   return (
     <Table<MemberRecord>
