1b46de7962294a1e6f01d7f14c44371737f7ecc6
yg <56058845+yunki-park@users.noreply.github.com>
2025-04-27 10:54:44 +0900

    [chore] 테마 내 primaryColor 오버라이드

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ConfigProvider } from 'antd';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { designTokens } from '@/styles/design-tokens';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: designTokens.colorPrimaryBase,
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>,
 );
